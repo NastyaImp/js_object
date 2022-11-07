9f6f8ae895e15c3c667062b0fd810248c9a84cbe
nastya <Imperiian@yandex.ru>
2022-11-07 22:08:19 +0300

    js-4-object

---
+++ task.js
@@ -0,0 +1,31 @@
+function Student(name, gender, age) {
+   this.name = name;
+   this.gender = gender;
+   this.age = age;
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+    this.marks = [mark]; 
+  } else {
+  this.marks.push(mark); 
+  }
+}
+
+Student.prototype.addMarks = function (...args) {
+  this.marks = args;
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject
+  delete this.marks;
+  this.excluded = reason;
+}
